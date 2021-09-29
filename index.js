5828398ee3382b0faef3e919f5294d49126e21b4
Zachary Sain <zachsain@gmail.com>
2021-09-28 21:41:19 -0500

    commit tree a1902e72ca9b1d12b0db73054148caa59dd4063e

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function fetchBooks() {
+
+fetch('https://anapioficeandfire.com/api/books')
+.then((resp) => resp.json())
+.then((json) => console.log(json))
   // To pass the tests, don't forget to return your fetch!
   
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
